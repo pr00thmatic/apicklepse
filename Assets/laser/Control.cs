4c428541075c4f0db19573941298051c997ab44f
VG <vengadoravg@gmail.com>
2018-04-09 12:08:18 -0400

    apicklepse :O

---
+++ Assets/laser/Control.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using System.Collections;
+
+namespace Laser {
+    public class Control : MonoBehaviour {
+        void Update () {
+            if (Input.GetMouseButtonDown)
+        }
+    }
+}
