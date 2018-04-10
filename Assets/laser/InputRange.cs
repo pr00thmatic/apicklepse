9e29568486726f51ca289fcb40b52704ed90afc9
VG <vengadoravg@gmail.com>
2018-04-10 18:29:49 -0400

    laser point added :D

--- Assets/laser/InputRange.cs
+++ Assets/laser/InputRange.cs
@@ -1,27 +1,25 @@
 using UnityEngine;
 using System.Collections;
 
-[System.Serializable]
-public class InputRange {
-    public float Value {
-        get {
-            return ((value + 1)/2) * (max-min) + min;
+namespace Laser {
+    [System.Serializable]
+    public class InputRange {
+        public float Value {
+            get {
+                return ((_value + 1)/2) * (max-min) + min;
+            }
+            set {
+                this._value = value;
+            }
         }
-        set {
-            this.value = value;
+        public float min;
+        public float max;
+        private float _value;
+
+        public InputRange (float value, float min, float max) {
+            this._value = value;
+            this.min = min;
+            this.max = max;
         }
-    }
-    public float min;
-    public float max;
-
-    public float value;
-
-    public float debug;
-
-    public InputRange (float value, float min, float max) {
-        this.value = value;
-        this.min = min;
-        this.max = max;
     }
 }
-
---
+++ Assets/laser/RayCaster.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using System.Collections;
+
+namespace Laser {
+    public class RayCaster : MonoBehaviour {
+        public float floatingTreshold = 0.2f;
+        public GameObject dot;
+
+        private Control _control;
+
+        void Start () {
+            _control = GetComponent<Control>();
+        }
+
+        void Update () {
+            RaycastHit hit;
+            Vector3 direction = _control.transform.forward;
+
+            if (Physics.Raycast(transform.position, direction,
+                                out hit, Mathf.Infinity,
+                                LayerMask.GetMask("laser surface"))) {
+                dot.transform.position = hit.point + hit.normal * floatingTreshold;
+                dot.transform.forward = hit.normal;
+            }
+        }
+    }
+}
